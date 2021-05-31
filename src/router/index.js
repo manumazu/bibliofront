cd5fb25af080ff29f92c62c08c361933867d8aba
ManuMazu <emmanuel.mazurier@gmail.com>
2021-05-31 19:01:03 +0200

    #1 move Books inside views directory corresponding to vue router organisation

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Books from '../components/Books.vue';
+import Books from '../views/Books.vue';
 
 Vue.use(VueRouter);
 
